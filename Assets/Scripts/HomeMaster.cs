ddf88f59ce781898145a679388493b0141cd8ff2
Mohammed Header <mohheader@hotmail.com>
2016-01-30 13:57:57 +0200

    Init Network && two players Scene

--- Assets/Scripts/HomeMaster.cs
+++ Assets/Scripts/HomeMaster.cs
@@ -4,7 +4,22 @@ using UnityEngine.SceneManagement;
 
 
 public class HomeMaster : MonoBehaviour {
+	RoomNetworkMaster roomNetwork;
+
+	void Awake(){
+		roomNetwork = GetComponent<RoomNetworkMaster> ();
+	}
+
 	public void StartOnePlayerGame(){
 		SceneManager.LoadScene ("Game_1Player");
+	}
+
+	public void StartTwoPlayersGame(){
+		roomNetwork.OnJoinedRoomEvent += GoToGame;
+		roomNetwork.Connect ();
+	}
+		
+	void GoToGame(){
+		roomNetwork.StartScene ();
 	}
 }
---
+++ Assets/Scripts/Network/RoomNetworkMaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomNetworkMaster : Photon.PunBehaviour {
+	public void Connect(){
+		PhotonNetwork.automaticallySyncScene = true;
+		PhotonNetwork.ConnectUsingSettings ("1");
+	}
+
+	public override void OnJoinedLobby(){
+		RoomOptions ro = new RoomOptions (){isVisible = true, maxPlayers = 2};
+		PhotonNetwork.JoinOrCreateRoom ("Random!", ro, TypedLobby.Default);
+	}
+
+	public override void OnJoinedRoom(){
+		if (PhotonNetwork.isMasterClient &&  OnJoinedRoomEvent != null)
+			OnJoinedRoomEvent ();
+	}
+
+	public void StartScene(){
+		PhotonNetwork.LoadLevel ("Game_2Players");
+	}
+
+	public System.Action OnJoinedRoomEvent;
+}
